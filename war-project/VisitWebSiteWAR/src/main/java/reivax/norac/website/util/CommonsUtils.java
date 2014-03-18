df53957716ff52600462627d09df71c69750fb4f
Xavier <Xavier@Reivax0zPC>
2014-03-19 10:23:35 +1100

    Change-Id: I4c13e00a2e30e3aab6612aedc81f190f54098c23

--- war-project/VisitWebSiteWAR/src/main/java/reivax/norac/website/util/CommonsUtils.java
+++ war-project/VisitWebSiteWAR/src/main/java/reivax/norac/website/util/CommonsUtils.java
@@ -4,24 +4,42 @@ import java.io.IOException;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 
 public class CommonsUtils {
 
 	public static FTPFile[] getFilesFromFTPServer(String directory){
-		FTPClient f = new FTPClient();
+		FTPClient ftp = new FTPClient();
 		try {
-			f.connect(Commons.FTP_SERVER, 21);
-			f.login(Commons.FTP_SERVER_USER, Commons.FTP_SERVER_PASS);
-			FTPFile[] files = f.listFiles(directory);
-			System.out.println(files); //Temp for debug
-			f.disconnect();
+			ftp.connect(Commons.FTP_SERVER, 21);
+			ftp.login(Commons.FTP_SERVER_USER, Commons.FTP_SERVER_PASS);
+			int reply = ftp.getReplyCode();
+
+			if(!FTPReply.isPositiveCompletion(reply)) {
+				System.err.println("FTP server refused connection.");
+				throw new Exception("Not able to connect");
+			}
+			
+			// get files
+			FTPFile[] files = ftp.listFiles(directory);
+
+			ftp.logout();
+
 			return files;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
+		} finally{
+			if(ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch(IOException ioe) {
+					// do nothing
+				}
+			}
 		}
 	}
-	
+
 	public static String getNameWithoutExtension(String name){
 		try{
 			String[] parts = name.split("\\.");
