84b649fcfa6276c991ebcd2caa7cb3fb58151d45
Xavier <Xavier@Reivax0zPC>
2014-06-10 19:06:25 +1000

    Change-Id: I5b42f7c99a31636039ae8dacf193d2c6137f9e0c

--- ejb-project/VisitWebSiteEJB/src/main/java/reivax/norac/website/caches/Cache.java
+++ ejb-project/VisitWebSiteEJB/src/main/java/reivax/norac/website/caches/Cache.java
@@ -25,7 +25,8 @@ public abstract class Cache<T> {
 	
 	private WebSiteEJB getWebSiteEJB() throws NamingException{
 	  InitialContext ctx = new InitialContext();
-	  return (WebSiteEJB)ctx.lookup("java:global/VisitWebSiteWAR-0.0.1-SNAPSHOT/WebSiteEJB!reivax.norac.website.service.WebSiteEJB");
+	  // For local tests: java:global/VisitWebSiteWAR-0.0.1-SNAPSHOT/WebSiteEJB!reivax.norac.website.service.WebSiteEJB
+	  return (WebSiteEJB)ctx.lookup("java:global/app/WebSiteEJB!reivax.norac.website.service.WebSiteEJB");
 	}
 	
 	protected List<T> elements;
